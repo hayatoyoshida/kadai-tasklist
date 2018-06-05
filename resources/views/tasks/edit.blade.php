cf3854f59e66ac924b11241f4236945f29b171f6
hayato.yoshida <ec2-user@ip-172-31-16-129.us-east-2.compute.internal>
2018-06-05 06:42:49 +0000

    Procfile

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -10,12 +10,12 @@
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 	<div class='form-group'>
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null ,['class' => 'form-control'])!!}
 	</div>
 
 	<div class='form-group'>
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
 	</div>
 
         {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
